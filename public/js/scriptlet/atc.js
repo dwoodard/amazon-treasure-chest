9b5f7cc6a7d07efcc8efa47f2e440b24f2b5732c
Dustin Woodard <dustin.woodard@gmail.com>
2015-04-12 20:15:01 -0600

    atc.js

--- public/js/scriptlet/atc.js
+++ public/js/scriptlet/atc.js
@@ -17,13 +17,13 @@
 
 
 // var script = document.createElement('script');script.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js";document.getElementsByTagName('head')[0].appendChild(script);
-var atc;
+
 jQuery( document ).ready(function($,atc) {
     console.log( "ready!" );
 
 
 
-
+    var bodySTR = jQuery('body').text();
 
 	var o = {};
 	o.url = window.location;
@@ -31,9 +31,10 @@ jQuery( document ).ready(function($,atc) {
 	o.product_productTitle = $("#productTitle").text();
 	o.product_price = $("#priceblock_ourprice").text();
 	o.product_madeBy = $("#brand").text();
+	o.product_madeByLink = $("#brand").attr('href');
 
 
-	o.customers_totalCustomerReviews = /[0-9]+(,[0-9]+)*/ig.exec($("#acrCustomerReviewText").text())[0];
+	o.customers_totalCustomerReviews = /(\d+\sreview(s)?)/ig.exec(bodySTR) ? /(\d+\sreview(s)?)/ig.exec(bodySTR)[0] : null;
 
 
 
@@ -52,37 +53,36 @@ jQuery( document ).ready(function($,atc) {
 	o.merchantInfo_SellerFBACount = 0;
 
 
-	o.product_details_dimensions = /Product Dimensions:\s+(.*)/ig.exec($("li:contains('Product Dimensions:')").text())[1];
-	o.product_details_shippingWeight = /Shipping Weight:\s+(.*)/ig.exec($("li:contains('Shipping Weight')").text().replace(' (View shipping rates and policies)', ''))[1];
-	o.product_details_ASIN = /ASIN: (.*)/ig.exec($("*:contains('ASIN:')").text())[1];
-	o.product_details_ItemModelNumber = /Item model number:\s+(.*)/ig.exec($("li:contains('Item model number:')").text())[1];
+	o.product_details_dimensions = /(\d+(\.\d{1,2})?)?\sx\s(\d+(\.\d{1,2})?)?\sx\s(\d+(\.\d{1,2})?)?\s(\w+)/ig.exec(bodySTR) ? /(\d+(\.\d{1,2})?)?\sx\s(\d+(\.\d{1,2})?)?\sx\s(\d+(\.\d{1,2})?)?\s(\w+)/ig.exec(bodySTR)[0] : null;
+	o.product_details_shippingWeight = /(Shipping Weight\s?.*\d+(\.\d{1,2})?\s(ounces|pounds))/ig.exec(bodySTR) ? /(Shipping Weight\s?.*\d+(\.\d{1,2})?\s(ounces|pounds))/ig.exec(bodySTR)[0].replace('Shipping Weight','') : null
+	o.product_details_ASIN = /(?:\b)((?=[0-9a-z]*\d)[0-9a-z]{10})(?:\b)/ig.exec(location.href)[0];
+	o.product_details_ItemModelNumber = /(Item model number.*)\w+/ig.exec(jQuery('body').text()) ? /(Item model number.*\w+)/ig.exec(jQuery('body').text())[0].replace('Item model number','') : null;
+	o.product_details_manufacturerPartNumber = /(Manufacturer Part Number.*\w+)/ig.exec(jQuery('body').text()) ? /(Manufacturer Part Number.*\w+)/ig.exec(jQuery('body').text())[0].replace('Manufacturer Part Number','') : null;
 
 
-	o.product_details_category_rank = /Amazon Best Sellers Rank:\s+(#\d+ in.* \()/ig.exec($("li:contains('Amazon Best Sellers Rank:')").text())[1].replace(' (', '');
-	o.product_details_category_category = /Amazon Best Sellers Rank:\s+(#\d+ in.* \()/ig.exec($("li:contains('Amazon Best Sellers Rank:')").text())[1].replace(' (', '');
+	o.product_details_category_rank = /(#[0-9]+(,[0-9]+)*) in (.*)\(See top/ig.exec(jQuery('body').text()) ? /(#[0-9]+(,[0-9]+)*) in (.*)\(See top/ig.exec(jQuery('body').text())[1] :null;
+	o.product_details_category_category = /(#[0-9]+(,[0-9]+)*) in (.*)\(See top/ig.exec(jQuery('body').text()) ? /(#[0-9]+(,[0-9]+)*) in (.*)\(See top/ig.exec(jQuery('body').text())[3] :null;
+
+	o.product_details_subcategory_category =  jQuery('.zg_hrsr_item').text().trim().replace(/\s\n/g, '').replace(/\n/g, ';').replace(/ +/g, ' ')
 
 
-	o.product_details_subcategory_rank = $(".zg_hrsr_rank").text();
-	o.product_details_subcategory_category =  $(".zg_hrsr_ladder").text();
-	o.product_details_subcategory_category_details = $(".zg_hrsr_ladder a");
 
-	atc = o;
 
 	//check for sells for fba
-	$.ajax(
-	{
-		url:o.merchantInfo_newSellers_link
-	})
-	.success(function(data){
-		var result = $(data).find('.olpOffer');
-		o.merchantInfo_SellerFBACount = result.length;
-		console.log(result);
-		console.log(o.merchantInfo_SellerFBACount);
+	// $.ajax(
+	// {
+	// 	url:o.merchantInfo_newSellers_link
+	// })
+	// .success(function(data){
+	// 	var result = $(data).find('.olpOffer');
+	// 	o.merchantInfo_SellerFBACount = result.length;
+	// 	console.log(result);
+	// 	console.log(o.merchantInfo_SellerFBACount);
 
 
-		$.post('http://dev.atc.dustinwoodard.net/scriptlet', o)
-		.done(function(data){
-			console.log(data)
-		})
-	});
+	// 	$.post('http://dev.atc.dustinwoodard.net/scriptlet', o)
+	// 	.done(function(data){
+	// 		console.log(data)
+	// 	})
+	// });
 });
