b14778707f2126a92be27ab1ad2fc955544fbf0d
Dustin Woodard <dustin.woodard@gmail.com>
2015-04-12 21:11:40 -0600

    atc.js

--- public/js/scriptlet/atc.js
+++ public/js/scriptlet/atc.js
@@ -40,7 +40,7 @@ jQuery( document ).ready(function($,atc) {
 
 	o.merchantInfo_hasMerchantInfo = $("#merchant-info a").length;
 	o.merchantInfo_newSellers_total = $("a[href*='condition=new']").parent().text().split('from')[0];
-	o.merchantInfo_newSellers_link = location.origin + $($("a[href*='condition=new']")[0]).attr('href') + "&shipPromoFilter=1";
+	o.merchantInfo_newSellers_link = jQuery("a[href*='condition=new']").length ? location.origin + jQuery(jQuery("a[href*='condition=new']")[0]).attr('href') + "&shipPromoFilter=1":null;
 	o.merchantInfo_newSellers_lowestSellingPrice = $("a[href*='condition=new']").parent().text().split('from')[1];
 	o.merchantInfo_usedSellers_total = $("a[href$='condition=used']").parent().text().split('from')[0];
 	o.merchantInfo_usedSellers_lowestSellingPrice = $("a[href$='condition=used']").parent().text().split('from')[1];
@@ -66,23 +66,38 @@ jQuery( document ).ready(function($,atc) {
 	o.product_details_subcategory_category =  jQuery('.zg_hrsr_item').text().trim().replace(/\s\n/g, '').replace(/\n/g, ';').replace(/ +/g, ' ')
 
 
+	console.log('check FBA users')
 
+
+	var postDataLink = 'http://dev.atc.dustinwoodard.net/scriptlet'
 
 	//check for sells for fba
-	// $.ajax(
-	// {
-	// 	url:o.merchantInfo_newSellers_link
-	// })
-	// .success(function(data){
-	// 	var result = $(data).find('.olpOffer');
-	// 	o.merchantInfo_SellerFBACount = result.length;
-	// 	console.log(result);
-	// 	console.log(o.merchantInfo_SellerFBACount);
+		jQuery.ajax({
+			url:o.merchantInfo_newSellers_link
+		}).error(function(data){
+			console.log(data);
+			o.merchantInfo_SellerFBACount = 'failed';
+
+			jQuery.post(postDataLink, o)
+			.done(function(data){
+				console.log(data)
+			})
+		}).success(function(data){
+			var result = $(data).find('.olpOffer');
+			o.merchantInfo_SellerFBACount = result.length;
+			console.log(result);
+			console.log(o.merchantInfo_SellerFBACount);
+			console.log('got FBA users')
+
+			jQuery.post(postDataLink, o)
+			.done(function(data){
+				console.log(data)
+			})
+		});
 
 
-	// 	$.post('http://dev.atc.dustinwoodard.net/scriptlet', o)
-	// 	.done(function(data){
-	// 		console.log(data)
-	// 	})
-	// });
+
+
+
+
 });
