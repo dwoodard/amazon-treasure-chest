c5a90fe225b44b6e208c310e08e1d16713c41df0
Dustin Woodard <dustin.woodard@gmail.com>
2015-04-28 01:05:28 -0600

    Got NUMBERS!!! yes!!!

--- public/scraps/sellers/scrape.php
+++ public/scraps/sellers/scrape.php
@@ -1,35 +1,35 @@
 <?php
-$expiration = 3600;
+
+
+$curl_options = [
+//        CURLOPT_HEADER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_PROXY => '0.0.0.0:9050',
+    CURLOPT_PROXYTYPE => CURLPROXY_SOCKS5,
+    CURLOPT_HTTPPROXYTUNNEL => 1,
+    CURLINFO_HEADER_OUT => true,
+    CURLOPT_FOLLOWLOCATION => false,
+    CURLOPT_AUTOREFERER => true,
+    CURLOPT_CONNECTTIMEOUT => 0,
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_COOKIESESSION => TRUE,
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_USERAGENT => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36',
+];
 
 function get_data($url)
 {
-    $ch = curl_init();
-    $timeout = 5;
+    $cookieFile = "./cookies/cookie" . uniqid() . ".txt";
+    global $curl_options;
 
-    // TOR
-    curl_setopt($ch, CURLOPT_PROXY, '0.0.0.0:9050');
-    curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
-    curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, 1);
+    $curl_options[CURLOPT_COOKIE] = $cookieFile;
+    $curl_options[CURLOPT_COOKIEFILE] = $cookieFile;
+    $curl_options[CURLOPT_COOKIEJAR] = $cookieFile;
+    $ch = curl_init($url);
 
-    curl_setopt($ch, CURLOPT_HEADER, 1);
-
-    curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
-    curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
-
-    curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-
-//    preg_match_all('|Set-Cookie: (.*);|U', $content, $results);
-//    $cookies = implode(';', $results[1]);
-//    curl_setopt($ch, CURLOPT_COOKIE,  $cookies);
-    $uid = uniqid();
-    curl_setopt($ch, CURLOPT_COOKIESESSION, TRUE);
-    curl_setopt($ch, CURLOPT_COOKIEFILE, "./cookies/cookie".$uid.".txt");	// set cookie file
-    curl_setopt($ch, CURLOPT_COOKIEJAR, './cookies/cookie'.$uid.'.txt');
-
-    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36');
+    curl_setopt_array($ch, $curl_options);
     $data = curl_exec($ch);
     curl_close($ch);
     return $data;
@@ -37,6 +37,9 @@ function get_data($url)
 
 function post_data($url, $fields)
 {
+    $cookieFile = "./cookies/cookie" . uniqid() . ".txt";
+    global $curl_options;
+
     //url-ify the data for the POST
     $fields_string = "";
     foreach ($fields as $key => $value) {
@@ -44,22 +47,45 @@ function post_data($url, $fields)
     }
     rtrim($fields_string, '&');
 
-    //open connection
-    $ch = curl_init();
-    //set the url, number of POST vars, POST data
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POST, count($fields));
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
 
-    $uid = uniqid();
-    curl_setopt($ch, CURLOPT_COOKIESESSION, TRUE);
-    curl_setopt($ch, CURLOPT_COOKIEFILE, "./cookies/cookie".$uid.".txt");	// set cookie file
-    curl_setopt($ch, CURLOPT_COOKIEJAR, './cookies/cookie'.$uid.'.txt');
+
+    //open connection
+    $ch = curl_init($url);
+
+    $curl_options[CURLOPT_COOKIE] = $cookieFile;
+    $curl_options[CURLOPT_COOKIEFILE] = $cookieFile;
+    $curl_options[CURLOPT_COOKIEJAR] = $cookieFile;
+
+    $curl_options[CURLOPT_POST] = count($fields);
+    $curl_options[CURLOPT_POSTFIELDS] = $fields_string;
+//    $curl_options[CURLOPT_POSTREDIR] = 1;
+
+
+    curl_setopt_array($ch, $curl_options);
 
     //execute post
     $data = curl_exec($ch);
 
-    //close connection
+    $rough_content = curl_exec($ch);
+    $err = curl_errno($ch);
+    $errmsg = curl_error($ch);
+    $header = curl_getinfo($ch);
     curl_close($ch);
-    return $data;
+
+    $header_content = substr($rough_content, 0, $header['header_size']);
+    $body_content = trim(str_replace($header_content, '', $rough_content));
+    $pattern = "#Set-Cookie:\\s+(?<cookie>[^=]+=[^;]+)#m";
+    preg_match_all($pattern, $header_content, $matches);
+    $cookiesOut = implode("; ", $matches['cookie']);
+
+    $header['errno'] = $err;
+    $header['errmsg'] = $errmsg;
+    $header['headers'] = $header_content;
+    $header['content'] = $body_content;
+    $header['cookies'] = $cookiesOut;
+    return [
+        "header" => $header,
+        "content" => $rough_content
+    ];
 }
+
--- public/scraps/sellers/sellers.php
+++ public/scraps/sellers/sellers.php
@@ -1,9 +1,8 @@
 <?php
-set_time_limit (0);
+set_time_limit(0);
 include 'simple_html_dom.php';
 include 'scrape.php';
 $sellerInfo = [];
-
 
 /** 1. Get List of products */
 
@@ -22,21 +21,20 @@ foreach ($products as $product) {
     /** get_data **/
 
     $data = trim(get_data($product->new_sellers_link));
+
 // die($data); //pipe out what.html
 // $str = file_get_contents ('what.html');
 
-
     /**
-    * 3. Find each Seller Row
-    *	Create an array of all sellers
-    * 	$sellers = $html->find('.olpOffer', 0)
-    */
-
+     * 3. Find each Seller Row
+     *    Create an array of all sellers
+     *    $sellers = $html->find('.olpOffer', 0)
+     */
 
     $html = str_get_html($data);
 //    $html = str_get_html($str);
 
-    if(!method_exists($html,'find')){
+    if (!method_exists($html, 'find')) {
         continue;
     }
     $sellers = $html->find('.olpOffer');
@@ -45,27 +43,30 @@ foreach ($products as $product) {
 //    die();
 
     /** 4. For Each Row as $seller
-    *	find:
-    *	sellerId = $sellers.find(".olpSellerName").a['href']
-    *	all others
-    **/
+     *    find:
+     *    sellerId = $sellers.find(".olpSellerName").a['href']
+     *    all others
+     **/
 
     foreach ($sellers as $key => $value) {
         $sellerInfo[$key] = [];
-        $sellerInfo[$key]['price'] = $html->find('.olpOfferPrice', 0)->plaintext;
+        $sellerInfo[$key]["asin"] = $product->asin;
+        $sellerInfo[$key]['price'] = $html->find('.olpOfferPrice', 0) ? $html->find('.olpOfferPrice', 0)->plaintext : "";
+//        print_r($sellerInfo[$key]['price']);
+//        die();
 
         $html = str_get_html($value);
 
         // olpSellerName
         foreach ($html->find('.olpSellerName') as $e) {
-            $olpSellerName = str_get_html($e)->find('a', 0);
-            // echo $olpSellerName->href;
-            if (isset($olpSellerName->href)) {
-                $sellerInfo[$key]['id'] = $olpSellerName->href;
-                $sellerInfo[$key]['link_to_seller_products'] = $olpSellerName->href;
-                $sellerInfo[$key]['name'] = $olpSellerName->plaintext;
+            $sellerIdLink = str_get_html($e)->find('a', 0);
+            if (isset($sellerIdLink->href)) {
+                preg_match("/(?:seller=|shops\/)([A-Z0-9]+)/", $sellerIdLink->href, $sellerId);
+                $sellerInfo[$key]['sellerId'] = $sellerId[1];
+                $sellerInfo[$key]['link_to_seller_products'] = $sellerIdLink->href;
+                $sellerInfo[$key]['name'] = $sellerIdLink->plaintext;
             } else {
-                $sellerInfo[$key]['id'] = 'amazon';
+                $sellerInfo[$key]['sellerId'] = 'amazon';
                 $sellerInfo[$key]['name'] = 'amazon';
             }
             $sellerInfo[$key]['stock'] = "";
@@ -84,30 +85,32 @@ foreach ($products as $product) {
             foreach ($inputs as $input) {
                 $fields[$input->name] = $input->value;
             }
+            $fields['itemCount:1'] = 999;
             $fields['quantity.1'] = 999;
             $sellerInfo[$key]['addToCartFields'] = $fields;
 
-
             //"Click" add to cart
-//            $cartAdded = post_data($action, $sellerInfo[$key]['addToCartFields']);
+            $cartAddedPage = post_data($action, $sellerInfo[$key]['addToCartFields']);
+
+            // POST Confirm click on add to cart
+            $html = str_get_html($cartAddedPage["content"]);
+            $items_in_stock =  $html->getElementById('hlb-cart-itemcount')? $html->getElementById('hlb-cart-itemcount')->plaintext : null;
+            $sellerInfo[$key]['items_in_stock'] = $items_in_stock;
 
             //"Get Value from Cart"
-//            $items_in_cart = json_decode(get_data("http://www.amazon.com/gp/navigation/ajax/dynamic-menu.html/".$fields['session-id']."?cartItems=cart"));
+            $items_in_cart = json_decode(get_data("http://www.amazon.com/gp/navigation/ajax/dynamic-menu.html/" . $fields['session-id'] . "?cartItems=cart"));
+            $sellerInfo[$key]['items_in_cart'] = $items_in_cart;
 
-//            $sellerInfo[$key]['items_in_cart'] = $items_in_cart;
 
 
             //"Delete from Cart"
 
-
             //Wait 5 seconds
             sleep(1);
-
-
         }
     }
 }
-	print_r($sellerInfo);
+echo json_encode($sellerInfo);
 
 
 
