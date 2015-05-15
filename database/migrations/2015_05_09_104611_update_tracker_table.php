64cd8df0c5073ebe3581c2b5fae808cdf8498490
Dustin Woodard <dustin.woodard@gmail.com>
2015-05-15 10:16:01 -0600

    fix name of class

--- database/migrations/2015_05_09_104611_update_tracker_table.php
+++ database/migrations/2015_05_09_104611_update_tracker_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateTracker extends Migration {
+class UpdateTrackerTable extends Migration {
 
 	/**
 	 * Run the migrations.
